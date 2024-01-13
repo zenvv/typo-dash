810423cd64edecc0e1b5f1fb5aa8f72b3d970eab
zenvv <zenn.devv@gmail.com>
2024-01-13 15:04:36 -0300

    .

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/nav";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/nav.tsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav className="w-full flex items-center justify-center p-8">
+      <h1 className="font-bold text-xl">typo-dash</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
